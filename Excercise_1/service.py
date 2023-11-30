9acdbcbb0c04c8bf9eb0006f57af598ab4551852
Diego Córdova <diego.cordovan@outlook.cl>
2023-11-30 12:15:02 -0300

    Add: API get-date created

--- Excercise_1/service.py
+++ Excercise_1/service.py
@@ -1,6 +1,17 @@
 import requests
+import fastapi
+import json
 import os
+from dotenv import load_dotenv 
 
+load_dotenv()
+
+app = fastapi.FastAPI()
+
+@app.get("/get-date")
 def get_date():
-    response = requests.get(os.environ.get('DATE_API_URL'))
-    return response.json()
+    url = os.environ.get('DATE_API_URL')
+    response = requests.get(url)
+    date = response.json()
+    return {"date": date['fecha_actual']}
+
