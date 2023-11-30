fb7217d9e5e0d3481e8520f2da9e3df888681ae1
Diego Córdova <diego.cordovan@outlook.cl>
2023-11-30 12:16:31 -0300

    Add: openAI API returns the current date, using function calling

--- Excercise_1/main.py
+++ Excercise_1/main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from service import get_date
 
 import os
 
@@ -18,20 +19,19 @@ class User(BaseModel):
     name: str
     message: str
 
-@app.post("/get-response")
-def get_response(user: User):
+@app.post("/send-response")
+def send_message(user: User):
 
     # Create a prompt
     behavior = f'Eres una agente virtual llamada MIA y tu objetivo es decir la fecha a {user.name}'
 
-    # Create a completion
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=[
+    # Setting the messages and tools
+    messages=[
             {"role": "system" , "content" : behavior},
             {"role": "user" , "content" : user.message}
-        ],
-        tools=[
+        ]
+    
+    tools=[
             {
                 "type": "function",
                 "function": {   
@@ -43,8 +43,40 @@ def get_response(user: User):
                     },
                 }
             }
-        ],
+        ]
+    
+    # Create a completion
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=messages,
+        tools=tools,
         tool_choice="auto"
     )
 
-    return response
+    response_messages = response.choices[0].message
+    tool_calls = response.choices[0].message.tool_calls
+
+    if tool_calls:
+
+        available_tools = {"get_date" : get_date()}
+
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            tool_to_call = available_tools[tool_name]
+            tool_response = tool_to_call
+            messages.append({
+                    "role": "system",
+                    "content": tool_response,
+            })
+
+        final_response = openai.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages
+        )
+        
+        return final_response
+
+    else:
+        return response_messages    
+        
+            
